a3a05c4259021aa7b837141be39edb1df2a13590
Bruno Gonçalves de Oliveira <brunojobs252@live.com>
2021-09-23 00:00:48 -0300

    Adicionando mais classes

--- src/main/java/com/dio/live/model/Usuario.java
+++ src/main/java/com/dio/live/model/Usuario.java
@@ -2,6 +2,10 @@ package com.dio.live.model;
 
 import lombok.*;
 
+import javax.persistence.ManyToOne;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -9,11 +13,25 @@ import lombok.*;
 @EqualsAndHashCode
 @Builder
 public class Usuario {
+
     private Long id;
+
+    @ManyToOne
     private CategoriaUsuario categoriaUsuario;
+
     private String nome;
+
+    @ManyToOne
     private Empresa empresa;
+
+    @ManyToOne
     private NivelAcesso nivelAcesso;
+
+    @ManyToOne
     private JornadaTrabalho jornadaTrabalho;
+
+    private BigDecimal tolerancia;
+    private LocalDateTime inicioJornada;
+    private LocalDateTime finalJornada;
 
 }

2a55666f95240fd8a513fefcffd563bed73c3713
Bruno Gonçalves de Oliveira <brunojobs252@live.com>
2021-09-22 23:31:17 -0300

    Adicionando mais classes

---
+++ src/main/java/com/dio/live/model/CategoriaUsuario.java
@@ -0,0 +1,16 @@
+package com.dio.live.model;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class CategoriaUsuario {
+
+    private long id;
+    private String descricao;
+}
